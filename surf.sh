1717cabe690f5f6e325d64c5a483902781e1b402
ksellesk <zhengdachuan200305@gmail.com>
2017-06-03 19:23:55 +0800

    Update surf.sh

--- surf.sh
+++ surf.sh
@@ -6,7 +6,7 @@ pingtime=`ping -c 1 baidu.com | grep "100% packet loss" |wc -l`
 
 if [[ $pingtime -eq 1 ]];then
 
-	python ruc.py -u "usernam" -p "password"
+	python ruc.py -u "username" -p "password"
 
 fi
 
